22f62df155f997ebeda6649b6b4a6376ec2d61bf
katerina <kokatrn@gmail.com>
2019-09-23 18:55:11 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+var gulp = require('gulp'), // Подключаем Gulp
+	sass = require('gulp-sass'), // Подключаем Sass пакет,
+	less = require('gulp-less'), // Подключаем Less пакет,
+	browserSync = require('browser-sync'), // Подключаем Browser Sync
+	uglify = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+	cssnano = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+	rename = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+	del = require('del'), // Подключаем библиотеку для удаления файлов и папок
+	imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+	pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+	cache = require('gulp-cache'), // Подключаем библиотеку кеширования
+	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+babel = require('gulp-babel'); // Babel
+include = require('gulp-file-include'); // Библиотека для инклюда файлов
+
+/*gulp.task('sass', function () { // Создаем таск Sass
+	return gulp.src('app/sass/*.scss') // Берем источник
+		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+		.pipe(cssnano()) // Сжимаемs
+		.pipe(gulp.dest('build/css')) // Выгружаем результата в папку build/css
+		.pipe(browserSync.reload({
+			stream: true
+		})) // Обновляем CSS на странице при изменении
+});*/
+gulp.task('less', function () { // Создаем таск Less
+	return gulp.src('app/less/*.less') // Берем источник
+		.pipe(less()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+		.pipe(cssnano()) // Сжимаемs
+		.pipe(gulp.dest('build/css')) // Выгружаем результата в папку build/css
+		.pipe(browserSync.reload({
+			stream: true
+		})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function () { // Создаем таск browser-sync
+	browserSync({ // Выполняем browserSync
+		server: { // Определяем параметры сервера
+			baseDir: 'build' // Директория для сервера - build
+		},
+		notify: false // Отключаем уведомления
+	});
+});
+
+gulp.task('scripts', function () {
+	return gulp.src([ // Берем все необходимые файлы js и исключаем файл с библиотеками
+		'app/js/*'
+	])
+		.pipe(uglify()) // Сжимаем JS файл
+		.pipe(gulp.dest('build/js')) // Выгружаем в папку build/js
+		.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('clean', function () {
+	return del.sync('build'); // Удаляем папку build перед сборкой
+});
+
+gulp.task('img', function () {
+	return gulp.src('app/img/**/*') // Берем все изображения из app
+		.pipe(cache(imagemin({ // С кешированием
+			// .pipe(imagemin({ // Сжимаем изображения без кеширования
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{ removeViewBox: false }],
+			use: [pngquant()]
+		}))/**/)
+		.pipe(gulp.dest('build/img')); // Выгружаем на продакшен
+});
+
+gulp.task('html', function () {
+	return gulp.src(['app/*.html'])
+		.pipe(include({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(gulp.dest('build'))
+		.pipe(browserSync.reload({ stream: true }))
+})
+
+// gulp.task('clear', function (callback) {
+// 	return cache.clearAll();
+// });
+
+/*gulp.task('build', ['clean', 'html', 'sass',  'scripts', 'img'])*/
+gulp.task('build', ['clean', 'html', 'less',  'scripts', 'img'])
+
+/*gulp.task('watch', ['browser-sync', 'build'], function () {
+	gulp.watch("app/sass/*.scss", ['sass']).on('change', browserSync.reload); // Наблюдение за SCSS файлами в корне проекта
+	gulp.watch('app/*.html', ['html'], browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch(['app/js/*.js'], ['scripts'], browserSync.reload);   // Наблюдение за JS файлами в папке js
+});
+*/
+gulp.task('watch', ['browser-sync', 'build'], function () {
+	gulp.watch("app/less/*.less", ['less']).on('change', browserSync.reload); // Наблюдение за SCSS файлами в корне проекта
+	gulp.watch('app/*.html', ['html'], browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch(['app/js/*.js'], ['scripts'], browserSync.reload);   // Наблюдение за JS файлами в папке js
+});
+
+gulp.task('default', ['watch']);
